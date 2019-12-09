e2daf13130fe8854c67ed88304d543dabe22a17f
atskae <atskae@gmail.com>
2019-12-09 15:53:43 -0500

    Copy constructor works?

--- metaprog/Array.hpp
+++ metaprog/Array.hpp
@@ -2,9 +2,18 @@
 #define CS540_ARRAY_H
 
 #include <tuple>
+#include <exception>
+#include <cassert>
+
 #include <stdio.h>
+#include <string.h>
 
 namespace cs540 {
+
+    // Out of range exception
+    struct OutOfRange : public std::exception {
+        const char* what() const throw() { return "Out of range exception."; }
+    }; // struct OutOfRange
 
     template<typename T, size_t Dim, size_t... Dims>
     class Array {
@@ -12,19 +21,60 @@ namespace cs540 {
             Array<T, Dims...> _array[Dim];
         public:
             Array() { printf("dim=%lu; sizeof(_array)=%lu\n", Dim, sizeof(_array)); }
+            
+            /* Copy constructor */
+            
+            Array(const Array<T, Dims...>& a) {
+                _array = Array(a[Dim]);
+                //_array[Dim] = a._array[Dim];
+                //assert(false);
+            }
+
+            //template<typename U>
+            //Array(const Array<U, Dims...>& a) {
+            //    assert(false);
+            //}
+
             ~Array() {}
+            
+            Array<T, Dims...>& operator[](int idx) {
+                if(idx < 0) throw OutOfRange();
+                if((size_t)idx > Dim) throw OutOfRange();
+
+                return _array[idx];
+            }
 
     }; // class Array
 
-    // recurive template ; base case
+    // class Array ; base case
     template<typename T, size_t Dim>
     class Array<T, Dim> {
         private:
-            T _array[Dim];
+            T _array[Dim] = {0};
         public:
             Array() { printf("dim=%lu; sizeof(_array)=%lu\n", Dim, sizeof(_array)); }
+
+            /* Copy constructor */
+ 
+            Array(const Array& a) {
+                printf("Base case copy constructor!\n");
+                memcpy(_array, a._array, sizeof(T) * Dim);
+            }
+
+            //template<typename U>
+            //Array(const Array<U, Dims...>& a) {
+            //
+            //}
+
             ~Array() {}
-    }; // class Array
+            
+            T& operator[](int idx) {
+                if(idx < 0) throw OutOfRange();
+                if((size_t)idx > Dim) throw OutOfRange();
+
+                return _array[idx];
+            }
+    }; // class Array ; base case
 
 } // namespace cs540
 
--- metaprog/main.cpp
+++ metaprog/main.cpp
@@ -5,6 +5,26 @@
 int main() {
     printf("Array\n");
     cs540::Array<int, 3, 4> a;
-    printf("sizeof(int)=%lu\n", sizeof(int));
+    printf("Array<int, 3, 4>\n");
+    
+    int x = 1;
+    int y = 2;
+    a[x][y] = 14;
+    printf("a[%i][%i]=%i\n", x, y, a[x][y]);
+   
+    // OutOfRange exception
+    try {
+        int num = a[100][2];
+        printf("Got num=%i\n", num);
+    } catch(const cs540::OutOfRange&) {
+        printf("Was hat passiert?\n");
+    }
+
+    // copy constructor
+    cs540::Array<int, 3, 4> b(a);
+    printf("b[%i][%i]=%i\n", x, y, b[x][y]);
+
+    printf("Aufwiedersehen.\n");
+ 
     return 0;
 }
